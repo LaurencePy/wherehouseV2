252913581650738ed64a532c081a4d60aaaee571
LaurenceBlueyy <la.eaton@gmail.com>
2023-08-18 19:23:57 +0100

    code reformat

--- stockprediction.py
+++ stockprediction.py
@@ -2,31 +2,52 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-tblsalesstatistics_data = pd.read_csv(r"C:\Users\laeat\Documents\Coding\wherehouse database stuff\tblSalesStatistics.csv")
-tblsales_data = pd.read_csv(r"C:\Users\laeat\Documents\Coding\wherehouse database stuff\tblSales.csv")
-tblitems_data = pd.read_csv(r"C:\Users\laeat\Documents\Coding\wherehouse database stuff\tblitems.csv")
-sales_predict_data = pd.read_csv(r"C:\Users\laeat\Documents\Coding\wherehouse database stuff\sales_predict_data.csv")
+tblsalesstatistics_data = pd.read_csv(
+    r"C:\Users\laeat\Documents\Coding\wherehouse database stuff\tblSalesStatistics.csv"
+)
+tblsales_data = pd.read_csv(
+    r"C:\Users\laeat\Documents\Coding\wherehouse database stuff\tblSales.csv"
+)
+tblitems_data = pd.read_csv(
+    r"C:\Users\laeat\Documents\Coding\wherehouse database stuff\tblitems.csv"
+)
+sales_predict_data = pd.read_csv(
+    r"C:\Users\laeat\Documents\Coding\wherehouse database stuff\sales_predict_data.csv"
+)
 
 tblsales_data["SaleDate"] = pd.to_datetime(tblsales_data["SaleDate"])
 tblitems_data["ExpiryDate"] = pd.to_datetime(tblitems_data["ExpiryDate"])
 
-tblsalesstatistics_data_extract = tblsalesstatistics_data[["ItemID", "SalesWeek", "SalesMonth"]]
+tblsalesstatistics_data_extract = tblsalesstatistics_data[
+    ["ItemID", "SalesWeek", "SalesMonth"]
+]
 tblitems_data_extract = tblitems_data[["ItemID", "ExpiryDate"]]
 
-tblitems_data_extract["days_to_expire"] = (tblitems_data["ExpiryDate"] - tblsales_data["SaleDate"]).dt.days
+tblitems_data_extract["days_to_expire"] = (
+    tblitems_data["ExpiryDate"] - tblsales_data["SaleDate"]
+).dt.days
 
 tblitems_days_to_expire_extract = tblitems_data_extract[["ItemID", "days_to_expire"]]
 
-first_merge = pd.merge(tblsalesstatistics_data_extract, tblitems_days_to_expire_extract, on="ItemID", how="left")
+first_merge = pd.merge(
+    tblsalesstatistics_data_extract,
+    tblitems_days_to_expire_extract,
+    on="ItemID",
+    how="left",
+)
 
 merged_data = first_merge.drop_duplicates(subset="ItemID", keep="first")
 concatenation_with_predict_data = pd.concat([merged_data, sales_predict_data], axis=1)
-normalise_data = concatenation_with_predict_data[["SalesWeek", "SalesMonth", "days_to_expire", "predict_sales"]]
+normalise_data = concatenation_with_predict_data[
+    ["SalesWeek", "SalesMonth", "days_to_expire", "predict_sales"]
+]
 avoid_data = merged_data["ItemID"]
 
 normalised_data = (normalise_data - normalise_data.mean()) / normalise_data.std()
 
-normalised2_data = pd.concat([avoid_data, normalised_data.reset_index(drop=True)], axis=1)
+normalised2_data = pd.concat(
+    [avoid_data, normalised_data.reset_index(drop=True)], axis=1
+)
 
 #################################################
 
@@ -37,15 +58,16 @@ ones = np.ones([inputs.shape[0], 1])
 inputs = np.concatenate((ones, inputs), axis=1)
 
 outputs = normalised2_data["predict_sales"].values
-coefficients = np.zeros([1, inputs.shape[1]]) 
+coefficients = np.zeros([1, inputs.shape[1]])
 
 learningrate = 0.01
 iterations = 1000
 
 
-def computeCost(inputs,outputs,coefficients):
-    sum1 = np.power(((inputs @ coefficients.T)-outputs),2)
-    return np.sum(sum1)/(2 * len(inputs))
+def computeCost(inputs, outputs, coefficients):
+    sum1 = np.power(((inputs @ coefficients.T) - outputs), 2)
+    return np.sum(sum1) / (2 * len(inputs))
+
 
 #################################################
 
@@ -54,9 +76,11 @@ def gradientDescent(inputs, outputs, coefficients, iterations, learningrate):
     costhistory = np.zeros(iterations)
     for i in range(iterations):
         predictionerrors = inputs @ coefficients.T - outputs
-        coefficients = coefficients - (learningrate / len(inputs)) * (predictionerrors.T @ inputs)
+        coefficients = coefficients - (learningrate / len(inputs)) * (
+            predictionerrors.T @ inputs
+        )
         costhistory[i] = computeCost(inputs, outputs, coefficients)
-    
+
     return coefficients, costhistory
 
 
