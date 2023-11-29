48efe376f8b7980375ace5adefe15c78ee310f8d
LaurenceBlueyy <la.eaton@gmail.com>
2023-11-29 21:02:32 +0000

    code commenting, viewstock cleanup

--- app/src/main/java/com/example/wherehouse/ViewStock.kt
+++ app/src/main/java/com/example/wherehouse/ViewStock.kt
@@ -9,6 +9,7 @@ import retrofit2.Callback
 import retrofit2.HttpException
 import retrofit2.Response
 
+
 class ViewStock : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,23 +18,36 @@ class ViewStock : AppCompatActivity() {
 
         val listView: ListView = findViewById(R.id.listView)
 
+        // API call using my RetrofitClient object to fetch tblitems data from the server
         val call: Call<List<DataModel>> = RetrofitClient.apiService.getTblItems()
 
+        /* Asynchronous API call to keep the app running while the call
+        is in progress alternatively to a synchronous API call
+         */
         call.enqueue(object : Callback<List<DataModel>> {
+
+            // when the API call is successful
             override fun onResponse(call: Call<List<DataModel>>, response: Response<List<DataModel>>) {
                 if (response.isSuccessful) {
+                    // Accessing the data received from the API response
                     val data: List<DataModel>? = response.body()
+
+                    // error checking that the data is not null
                     data?.let {
+                        // Logging the API's data response for me to test the data response
                         Log.d("API_RESPONSE", "Response: $data")
 
+                        // Creating and setting up the adapter to display the data in the ListView
                         val adapter = ListAdapter(this@ViewStock, R.layout.list_item_layout, it)
                         listView.adapter = adapter
                     }
                 } else {
+                    // Logs the error code to the Logcat panel so that I can identify errors and debug the program
                     Log.e("API_RESPONSE", "API call failed with code ${response.code()}")
                 }
             }
 
+            // when the API call is unsuccessful, I implemented more error handling and error logging for debugging uses
             override fun onFailure(call: Call<List<DataModel>>, t: Throwable) {
                 try {
                     val errorResponse = (t as HttpException).response()?.errorBody()?.string()
