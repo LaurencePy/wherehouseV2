53345579d0ebb9cbb3c8afaedbd7b2ca20bd8e89
LaurenceBlueyy <la.eaton@gmail.com>
2024-01-14 11:29:28 +0000

    creating new data models

---
+++ app/src/main/java/com/example/wherehouse/DataModelTblSales.kt
@@ -0,0 +1,6 @@
+package com.example.wherehouse
+import com.google.gson.annotations.SerializedName
+data class DataModelTblSales (
+        @SerializedName("ItemID")
+        val itemId: Int,
+        )
